171cc724ae4f074f28dcba5b367300c37d07e7ba
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-08-20 20:24:39 -0300

    back button

--- src/pages/PokemonDetailsPage.tsx
+++ src/pages/PokemonDetailsPage.tsx
@@ -1,16 +1,19 @@
 import {useGetPokemonsByItsId} from "@/hooks/useGetPokemonsByitsId.ts";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {Button} from "@/components/shadcn/button.tsx";
 
 
 export function PokemonDetailsPage() {
     const { id } = useParams<{ id: string }>();
     const { isPending, data } = useGetPokemonsByItsId(Number(id));
+    const navigate = useNavigate();
 
     if (isPending) return <p className="text-center">Loading...</p>;
     if (!data) return <p className="text-center">Pokémon not found</p>;
 
     return (
         <div className="flex flex-col items-center p-6">
+            <Button onClick={() => navigate(`/`)}> back</Button>
             <h1 className="text-3xl font-bold mb-4 capitalize">{data.name}</h1>
             <img
                 src={data.imageUrl}
